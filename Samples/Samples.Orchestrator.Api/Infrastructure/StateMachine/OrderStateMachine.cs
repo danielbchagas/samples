cbc3071b0afa6be53e81391a4303be2c6bf5b7df
Daniel Chagas <daniel.boasq@gmail.com>
2024-10-26 22:17:49 -0300

    Update OrderStateMachine.cs

--- Samples/Samples.Orchestrator.Api/Infrastructure/StateMachine/OrderStateMachine.cs
+++ Samples/Samples.Orchestrator.Api/Infrastructure/StateMachine/OrderStateMachine.cs
@@ -4,87 +4,73 @@ namespace Samples.Orchestrator.Api.Infrastructure.StateMachine;
 
 public class OrderStateMachine : MassTransitStateMachine<OrderState>
 {
-    public State Submitted { get; private set;}
-    public State Accepted { get; private set;}
-    public State Cancelled { get; private set;}
-    public State Rollback { get; private set;}
-    
+    public State Submitted { get; private set; }
+    public State ReadyForShipping { get; private set; }
+    public State Accepted { get; private set; }
+    public State Cancelled { get; private set; }
+    public State Rollback { get; private set; }
+
     public Event<Domain.Events.Payment.Submitted> PaymentSubmitted { get; private set; }
-    public Event<Domain.Events.Payment.Accepted> PaymentAccepted { get; private set;}
-    public Event<Domain.Events.Payment.Cancelled> PaymentCancelled { get; private set;}
-    public Event<Domain.Events.Payment.Rollback> PaymentRollback { get; private set;}
-    
-    public Event<Domain.Events.Shipping.Submitted> ShippingSubmitted { get; private set;}
-    public Event<Domain.Events.Shipping.Accepted> ShippingAccepted { get; private set;}
-    public Event<Domain.Events.Shipping.Cancelled> ShippingCancelled { get; private set;}
-    public Event<Domain.Events.Shipping.Rollback> ShippingRollback { get; private set;}
+    public Event<Domain.Events.Payment.Accepted> PaymentAccepted { get; private set; }
+    public Event<Domain.Events.Payment.Cancelled> PaymentCancelled { get; private set; }
+    public Event<Domain.Events.Payment.Rollback> PaymentRollback { get; private set; }
+
+    public Event<Domain.Events.Shipping.Submitted> ShippingSubmitted { get; private set; }
+    public Event<Domain.Events.Shipping.Accepted> ShippingAccepted { get; private set; }
+    public Event<Domain.Events.Shipping.Cancelled> ShippingCancelled { get; private set; }
+    public Event<Domain.Events.Shipping.Rollback> ShippingRollback { get; private set; }
 
     public OrderStateMachine()
     {
         InstanceState(x => x.CurrentState);
-        
-        #region Payment
 
         Initially(
             When(PaymentSubmitted)
                 .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
-                .TransitionTo(Accepted)
-        );
-        
-        During(Accepted,
-            When(PaymentAccepted)
-                .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
                 .TransitionTo(Submitted)
         );
-        
-        During(Accepted,
+
+        During(Submitted,
+            When(PaymentAccepted)
+                .ThenAsync(async context =>
+                {
+                    context.Saga.CorrelationId = context.Message.CorrelationId;
+                    await context.Publish(new Domain.Events.Shipping.Submitted
+                    {
+                        CorrelationId = context.Message.CorrelationId
+                    });
+                })
+                .TransitionTo(ReadyForShipping)
+        );
+
+        During(ReadyForShipping,
+            When(ShippingAccepted)
+                .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
+                .TransitionTo(Accepted)
+        );
+
+        During(Submitted,
             When(PaymentCancelled)
                 .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
                 .TransitionTo(Cancelled)
         );
-        
-        During(Accepted,
+
+        During(Submitted,
             When(PaymentRollback)
                 .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
                 .TransitionTo(Rollback)
         );
 
-        #endregion
-
-        #region Shipping
-
-        During(Submitted,
-            When(PaymentAccepted)
-                .ThenAsync(context =>
-                {
-                    context.Saga.CorrelationId = context.Message.CorrelationId;
-                    
-                    return context.Publish(new Domain.Events.Shipping.Submitted()
-                    {
-                        CorrelationId = context.Message.CorrelationId
-                    });
-                })
-                .TransitionTo(Accepted)
-        );
-        
-        During(Accepted,
-            When(ShippingAccepted)
-                .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
-                .TransitionTo(Submitted)
-        );
-        
-        During(Accepted,
+        During(ReadyForShipping,
             When(ShippingCancelled)
                 .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
                 .TransitionTo(Cancelled)
         );
-        
-        During(Accepted,
+
+        During(ReadyForShipping,
             When(ShippingRollback)
                 .Then(context => context.Saga.CorrelationId = context.Message.CorrelationId)
                 .TransitionTo(Rollback)
         );
-
-        #endregion
     }
 }
