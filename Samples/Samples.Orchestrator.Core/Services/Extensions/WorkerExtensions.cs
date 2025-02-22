596bb0b4f0ddf9d88b868750d6e55cef20ef0917
Daniel Chagas <daniel.boasq@gmail.com>
2025-02-22 07:58:03 -0300

    servicos removidos

--- Samples/Samples.Orchestrator.Core/Services/Extensions/WorkerExtensions.cs
+++ Samples/Samples.Orchestrator.Core/Services/Extensions/WorkerExtensions.cs
@@ -7,9 +7,6 @@ public static class WorkerExtensions
     public static IServiceCollection AddWorker(this IServiceCollection services)
     {
         services.AddHostedService<PaymentWorker.PaymentWorker>();
-
-        services.AddScoped<PaymentWorker.AcceptedWorker>();
-        services.AddScoped<PaymentWorker.CancelledWorker>();
         
         return services;
     }
